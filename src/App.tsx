2ffe311834e39ef8676fbdb48f2e2f75b92be3cd
hyebin <sksp4334@naver.com>
2022-05-30 04:59:09 +0900

    feat: App에 clubs rotuer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Clubs from "./pages/Clubs/Clubs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/clubs/:clubId" element={<Clubs />} />
       </Routes>
     </BrowserRouter>
   );
