e9933e93dbe5c301805f7fd8a2775c9db7684f67
hyebin <sksp4334@naver.com>
2022-05-29 22:27:41 +0900

    feat: theme color 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/global";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    color: {
+      main: string;
+      grey: string;
+    };
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const color = {
+  main: "rgb(255, 121, 0)",
+  grey: "rgb(110,110,108)",
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
