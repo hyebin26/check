66abe156edb7676b11f245dcf53576871a18d489
hyebin <sksp4334@naver.com>
2022-05-30 14:55:10 +0900

    feat: useDate에 hour, minute 계산 추가

--- src/utils/useDate.ts
+++ src/utils/useDate.ts
@@ -4,11 +4,12 @@ const useDate = (time: string) => {
   const month = day.getMonth() + 1;
   const date = day.getDate() === 1 ? 31 : day.getDate() - 1;
   const week = WEEKDAY[day.getDay()];
-  const hours = day.getUTCHours();
-  const minute = day.getMinutes();
-  return `${month}/${date}(${week}) ${hours}:${minute} ~ ${
-    hours + 2
-  }:${minute}`;
+  const hours =
+    day.getUTCHours() > 10 ? day.getUTCHours() : `0${day.getUTCHours()}`;
+
+  const minute =
+    day.getMinutes() > 10 ? day.getMinutes() : `0${day.getMinutes()}`;
+  return `${month}/${date}(${week}) ${hours}:${minute} ~ ${hours}:${minute}`;
 };
 
 export default useDate;
