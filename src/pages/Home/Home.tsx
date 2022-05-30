54aebe04a4c58393a0bdd2884046383695a22ff1
hyebin <sksp4334@naver.com>
2022-05-30 14:59:39 +0900

    remove: Home에 사용하지 않은 것들 삭제

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,5 @@
-import React, { useEffect, useState } from "react";
-import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
-import getClubData from "../../api/getClubData";
-import ClubDataType from "../../types/clubDataType";
+import React from "react";
 import Search from "./Search/Search";
-import ClubList from "./ClubList/ClubList";
 import ClubContent from "./ClubContent/ClubContent";
 
 function Home() {
